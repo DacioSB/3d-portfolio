4a3cd9f0300af45e0bf9542d4d564fdb6aba5a34
DacioSB <dacio.bezerra@ccc.ufcg.edu.br>
2024-11-20 00:30:34 -0300

    :bento: finish hero section and update background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,16 @@ const Hero = () => {
               className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
           >
               <div className="flex flex-col justify-center items-center mt-5">
-                  <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+                  <div className="w-5 h-5 rounded-full bg-[#6141ac]" />
                   <div className="w-1 sm:h-80 h-40 violet-gradient" />
               </div>
 
               <div>
                   <h1 className={`${styles.heroHeadText} text-white`}>
-                      Hey there! I'm <span className="text-[#915EFF]">Dacio</span>
+                      {"Hey there! I'm"} <span className="text-[#6141ac]">Dacio</span>
                   </h1>
                   <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                      I'm a software developer', {" "}
+                      {"I'm a software developer"}
                       <br className="sm:block hidden" />
                       I create web applications and intensive data systems
                   </p>
